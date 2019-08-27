913df2296109c007ef8032ed4570b2b3ca392116
MohanLalPR <mohanlal020698@gmail.com>
2019-08-27 14:14:16 +0530

    reactiveforms

---
+++ myFirstApp/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './employee/create-employee.component';
+import { ListEmployeesComponent } from './employee/list-employees.component';
+import { from } from 'rxjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateEmployeeComponent,
+    ListEmployeesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
